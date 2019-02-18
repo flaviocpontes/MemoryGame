12ba54752a5742c838188e9394cae599aded1208
flaviocpontes <flaviocpontes@gmail.com>
2019-02-17 22:24:55 -0300

    Matching logic working

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
+let config = {
+    hideDelay: 500
+};
+
 let gameState = {
     board: [
-        {symbolClass: "fa-diamond", matched: false, revealed: false},
-        {symbolClass: "fa-diamond", matched: false, revealed: false},
-        {symbolClass: "fa-paper-plane-o", matched: false, revealed: false},
-        {symbolClass: "fa-paper-plane-o", matched: false, revealed: false},
-        {symbolClass: "fa-anchor", matched: false, revealed: false},
-        {symbolClass: "fa-anchor", matched: false, revealed: false},
-        {symbolClass: "fa-bolt", matched: false, revealed: false},
-        {symbolClass: "fa-bolt", matched: false, revealed: false},
-        {symbolClass: "fa-cube", matched: false, revealed: false},
-        {symbolClass: "fa-cube", matched: false, revealed: false},
-        {symbolClass: "fa-leaf", matched: false, revealed: false},
-        {symbolClass: "fa-leaf", matched: false, revealed: false},
-        {symbolClass: "fa-bicycle", matched: false, revealed: false},
-        {symbolClass: "fa-bicycle", matched: false, revealed: false},
-        {symbolClass: "fa-bomb", matched: false, revealed: false},
-        {symbolClass: "fa-bomb", matched: false, revealed: false}
+        {symbolClass: "fa-diamond", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-diamond", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-paper-plane-o", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-paper-plane-o", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-anchor", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-anchor", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bolt", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bolt", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-cube", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-cube", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-leaf", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-leaf", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bicycle", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bicycle", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bomb", matched: false, revealed: false, index: null},
+        {symbolClass: "fa-bomb", matched: false, revealed: false, index: null}
     ],
-    firstCard: null,
-    secondCard: null
+    revealed: null
 };
 
 function appendCardToBoard(board, card, index){
     let cardElement = '<li id="card' + index + '" class="card"><i class="fa ' + card.symbolClass + '"></i></li>';
-
+    card.index = index;
     board.append(cardElement)
 }
 
@@ -42,28 +45,68 @@ function shuffle(array) {
     return array;
 }
 
-function updateBoard(clickedCard) {
-    let cardIndex = Number(clickedCard.id.slice(4,6));
-    let stateCard = gameState.board[cardIndex];
-    stateCard.revealed = true;
-
-    if (stateCard.revealed === true) {
+function revealCard(clickedCard, stateCard) {
+    if (!stateCard.revealed === true) {
+        stateCard.revealed = true;
         $(clickedCard).addClass("open show");
     }
 }
 
+function hideCard(card) {
+    card.revealed = false;
+    $($('#card' + card.index)[0]).removeClass("open show");
+}
 
-function initBoard(gamestate) {
-    let board = $('.deck');
+
+function populateBoard(gamestate, board) {
     shuffle(gameState.board);
-
     gamestate.board.forEach(function (card, index) {
         appendCardToBoard(board, card, index)
     });
+}
+
+function secondCard(gamestate) {
+    return gamestate.revealed !== null;
+}
+
+function matchCards(stateCard, revealedCard) {
+    stateCard.matched = true;
+    revealedCard.matched = true;
+    $($('#card' + stateCard.index)[0]).addClass('match');
+    $($('#card' + revealedCard.index)[0]).addClass('match');
+}
+
+function hideCards(revealedCard, stateCard) {
+    setTimeout(function () {
+        hideCard(revealedCard);
+        hideCard(stateCard);
+    }, config.hideDelay);
+}
+
+function try_match(gamestate, stateCard) {
+    revealedCard = gamestate.revealed;
+    if (stateCard.symbolClass === revealedCard.symbolClass) {
+        matchCards(stateCard, revealedCard);
+    } else {
+        hideCards(revealedCard, stateCard);
+    }
+}
+
+function initBoard(gamestate) {
+    let board = $('.deck');
+
+    populateBoard(gamestate, board);
 
     board.on('click', 'li', function(){
-        let clickedCard = $(this)[0];
-        updateBoard(clickedCard);
+        let boardCard = $(this)[0];
+        let stateCard = gameState.board[Number(boardCard.id.slice(4, 6))];
+        revealCard(boardCard, stateCard);
+        if (secondCard(gamestate)) {
+            try_match(gamestate, stateCard);
+            gamestate.revealed = null;
+        } else {
+            gameState.revealed = stateCard;
+        }
     });
 
 }
